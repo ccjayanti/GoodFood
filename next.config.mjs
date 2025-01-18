4f5cc05c43a49cb55372636c40e322341ea52251
ccjayanti <cc.jayantibalmuchu@gmail.com>
2025-01-18 23:31:17 +0530

    image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+      unoptimized: true,
       remotePatterns: [
         {
           protocol: 'https', // Protocol of the domain (e.g., 'https')
